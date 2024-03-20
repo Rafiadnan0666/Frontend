5e579b33bc472d021d80a475999ffa0142a12217
Rafiadnan06666 <fn2344561@gmail.com>
2024-03-20 11:08:56 +0700

    first commit

---
+++ pertemuan1/object.js
@@ -0,0 +1,10 @@
+//membuat object : {}
+
+const user = {
+    name: "Ucup",
+    age : 20,
+    major : "Web Development"
+}
+// console.log(user.name);
+
+for(const key in user ) console.log(user[key]);
